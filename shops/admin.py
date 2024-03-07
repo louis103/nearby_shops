a631a28bf035998de2134308be4c4d33a12872c3
louis@programmer <62738677+louis103@users.noreply.github.com>
2024-03-07 19:16:21 +0300

    Push 001

---
+++ shops/admin.py
@@ -0,0 +1,11 @@
+# Register your models here.
+
+from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Shop
+from leaflet.admin import LeafletGeoAdmin
+
+
+@admin.register(Shop)
+class ShopAdmin(LeafletGeoAdmin):
+    list_display = ("name", "location", "address", "city")
